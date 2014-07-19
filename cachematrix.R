0993dc32bf3b00b7b696f1b37008ddef8cb90e67
MarkACohen <mark@cassconsulting.biz>
2014-07-19 09:33:34 -0400

    first cut, appears to work, need to add more comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,63 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) 
+{
+    # 'private' data member representing the matrix inverse...
+    inv <- NULL
 
+    # methods to get and set the inverse for this matrix...
+    getInv <- function()
+    {
+        inv    
+    }
+    setInv <- function(i)
+    {
+        inv <<- i
+    }
+        
+    # methods to get and set the matrix...
+    getMatrix <- function()
+    {
+        x
+    }
+    setMatrix <- function(y) 
+    {
+        x <<- y
+        
+        # clear the inv when we change the matrix...
+        inv <<- NULL 
+    }
+    
+    # build a list that consits of the methods that operate
+    # on our new matrix type...
+    list(setMatrix = setMatrix, getMatrix = getMatrix, 
+         setInv = setInv, getInv = getInv)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+    # check to see if the matrix already has its inverse cached...
+    inv <- x$getInv()
+    
+    # if the inverse is cached, return it...
+    if (!is.null(inv))
+    {
+        message("inv already cached!")
+        return (inv)
+    }
+    # otherwise calculate it, store it, and return it...
+    else
+    {        
+        message("inv not cached, calculating...")
+        
+        mat <- x$getMatrix()
+        inv <- solve(mat)
+        x$setInv(inv)
+        
+        return(inv)
+    }
 }
