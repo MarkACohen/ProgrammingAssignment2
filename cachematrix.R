1a1d95ed34672de7dd22190cbf61d91f72ebd017
MarkACohen <mark@cassconsulting.biz>
2014-07-19 09:44:54 -0400

    comments added, code is ready for submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains code that wraps a matrix around a set 
+## of data and functions that will calculate the inverse
+## of the matrix and then cache it.  Subsequent calls to
+## calculate the inverse will return the cached inverse.
+## If the matrix is set to a new matrix, the cache is cleared.
+##
+## To test these classes you can execute the following:
+##  m<-cbind(0:1, 1:0)
+##  superM<-makeCacheMatrix(m)
+##  cacheSolve(superM)
+##  cacheSolve(superM)
 
-## Write a short comment describing this function
-
+## Returns a list backed by the matrix argument (x).  The list 
+## contains functions that allow you to get/set the matrix, and 
+## get/set the inverse of the matrix.  Use cacheSolve to 
+## retrieve the inverse of the matrix that is contained in 
+## the returned list.
 makeCacheMatrix <- function(x = matrix()) 
 {
     # 'private' data member representing the matrix inverse...
@@ -38,8 +50,14 @@ makeCacheMatrix <- function(x = matrix())
 }
 
 
-## Write a short comment describing this function
-
+## This function assumes that the argument x is a 'CacheMatrix' 
+## that was created using the makeCacheMatrix function.  This 
+## function returns the inverse of the matrix contained in the 
+## specified 'CacheMatrix'.  The inverse will be cached, so that
+## it is only calculated once.  Subsequent calls to this method
+## with the same 'CacheMatrix' object will return the cached
+## inverse value.  This function assumes that the matrix is 
+## non singular and therefore has a valid inverse.
 cacheSolve <- function(x, ...) 
 {
     # check to see if the matrix already has its inverse cached...
